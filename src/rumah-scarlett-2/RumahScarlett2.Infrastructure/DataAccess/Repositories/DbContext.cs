263acf205f27ba1a3e7af39dea690b5e0c68ca79
Risal Walangadi <risal.walangadi@gmail.com>
2020-03-21 16:41:15 +0800

    Add data access

---
+++ src/rumah-scarlett-2/RumahScarlett2.Infrastructure/DataAccess/Repositories/DbContext.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper.Contrib;
+using System.Configuration;
+using RumahScarlett2.CommonComponents;
+using System.Data.SqlClient;
+
+namespace RumahScarlett2.Infrastructure.DataAccess.Repositories
+{
+  public class DbContext : IDisposable
+  {
+    private IDbConnection _conn;
+    private IDbTransaction _transaction;
+    private readonly string _connString;
+    private static readonly string _providerName = @"SQLitePCLRaw.core";
+
+    public IDbConnection Conn
+    {
+      get { return _conn ?? (_conn = GetOpenConnection(_connString, _providerName)); }
+    }
+
+    public IDbTransaction Transaction
+    {
+      get { return _transaction; }
+    }
+
+    public DbContext()
+    {
+      _connString = $@"Data Source={Environment.CurrentDirectory}\db_rs.db;";
+
+      if (_conn == null)
+      {
+        _conn = GetOpenConnection(_connString, _providerName);
+      }
+    }
+
+    public DbContext(string location, string file)
+    {
+      location = !string.IsNullOrWhiteSpace(location) ? location : "Environment.CurrentDirectory";
+      file = !string.IsNullOrWhiteSpace(file) ? file : "db_rs.db";
+
+      _connString = $@"Data Source={location}\{file};";
+
+      if (_conn == null)
+      {
+        _conn = GetOpenConnection(_connString, _providerName);
+      }
+    }
+
+    private IDbConnection GetOpenConnection(string connString, string providerName)
+    {
+      IDbConnection conn = null;
+      var dataAccessStatus = new DataAccessStatus();
+
+      try
+      {
+        var provider = DbProviderFactories.GetFactory(providerName);
+        conn = provider.CreateConnection();
+        conn.ConnectionString = connString;
+        conn.Open();
+      }
+      catch (SqlException ex)
+      {
+        dataAccessStatus.SetValues(status: "Error", operationSucceeded: false, exceptionMessage: ex.Message,
+                                   customMessage: "Tidak dapat membuka koneksi ke Database.\n" +
+                                   "Periksa data Database, Port, User dan Password database.",
+                                   helpLink: ex.HelpLink, errorCode: ex.ErrorCode, stackTrace: ex.StackTrace);
+        throw new DataAccessException(message: ex.Message, innerException: ex.InnerException,
+                                      dataAccessStatus: dataAccessStatus);
+      }
+
+      return conn;
+    }
+
+    public bool TesKoneksi()
+    {
+      var isConnected = false;
+
+      try
+      {
+        using (var conn = GetOpenConnection(_connString, _providerName))
+        {
+          isConnected = conn.State == ConnectionState.Open;
+        }
+      }
+      catch
+      {
+      }
+
+      return isConnected;
+    }
+
+    public void BeginTransaction(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted)
+    {
+      if (_transaction == null)
+      {
+        _transaction = Conn.BeginTransaction(isolationLevel);
+      }
+    }
+
+    public void Commit()
+    {
+      if (_transaction != null)
+      {
+        _transaction.Commit();
+        _transaction = null;
+      }
+    }
+
+    public void RollBack()
+    {
+      if (_transaction != null)
+      {
+        _transaction.Rollback();
+        _transaction = null;
+      }
+    }
+
+    public void Dispose()
+    {
+      if (_conn != null)
+      {
+        try
+        {
+          if (_conn.State == ConnectionState.Open)
+          {
+            RollBack();
+            _conn.Close();
+          }
+        }
+        finally
+        {
+          _conn.Dispose();
+        }
+      }
+
+      GC.SuppressFinalize(this);
+    }
+  }
+}
